257f88ebcaccf24d88648283157567e43c3fe80c
David <david.windler@windowslive.com>
2024-12-10 22:50:14 +0100

    solution day 09 part 2

--- 09/main.go
+++ 09/main.go
@@ -10,21 +10,44 @@ import (
 	"strings"
 )
 
+type FreePosition struct {
+	index   int
+	lengths int
+}
+
+type FilePosition struct {
+	index   int
+	value   int
+	lengths int
+}
+
+func (fp FreePosition) String() string {
+	return fmt.Sprintf("index %d, lengths %d;", fp.index, fp.lengths)
+}
+
+func (fp FilePosition) String() string {
+	return fmt.Sprintf("index %d, value %d, lengths %d;", fp.index, fp.value, fp.lengths)
+}
+
 type Disk struct {
-	diskMap       []int
-	fileBlocks    []string
-	freePositions []int
-	checksum      int
+	diskMap             []int
+	fileBlocks          []string       // Part 1
+	filePositions       []FilePosition // Part 2
+	freePositionIndices []int          // Part 1
+	freePositions       []FreePosition // Part 2
+	checksum            int
 }
 
 func main() {
 	disk := Disk{
-		diskMap:       make([]int, 0),
-		fileBlocks:    make([]string, 0),
-		freePositions: make([]int, 0),
+		diskMap:             make([]int, 0),
+		fileBlocks:          make([]string, 0),
+		freePositionIndices: make([]int, 0),
+		freePositions:       make([]FreePosition, 0),
+		filePositions:       make([]FilePosition, 0),
 	}
-	err := disk.readDiskMap("./input-small.txt")
 
+	err := disk.readDiskMap("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,40 +66,70 @@ func main() {
 func (m *Disk) log() {
 	// fmt.Println(m.diskMap)
 	// fmt.Println(m.fileBlocks)
+	// fmt.Println(m.freePositionIndices)
 	// fmt.Println(m.freePositions)
+	// fmt.Println(m.filePositions)
 	fmt.Println("Checksum: ", m.checksum)
 }
 
+// Part 1
+// func (m *Disk) moveFileBlocks() {
+// 	freePositionsIndex := 0
+// 	numberOfFreePositions := len(m.freePositionIndices)
+
+// 	for i := len(m.fileBlocks) - 1; i >= 0; i-- {
+// 		if m.fileBlocks[i] == "." {
+// 			continue
+// 		}
+
+// 		if freePositionsIndex+1 == numberOfFreePositions {
+// 			return
+// 		}
+
+// 		nextFreePosition := m.freePositionIndices[freePositionsIndex]
+// 		if nextFreePosition > i {
+// 			return
+// 		}
+
+// 		val := m.fileBlocks[i]
+// 		m.fileBlocks[nextFreePosition] = val
+// 		m.fileBlocks[i] = "."
+
+// 		freePositionsIndex++
+// 	}
+// }
+
+// Part 2
 func (m *Disk) moveFileBlocks() {
-	freePositionsIndex := 0
-	numberOfFreePositions := len(m.freePositions)
+	for i := len(m.filePositions) - 1; i >= 0; i-- {
+		fileToMove := m.filePositions[i]
 
-	for i := len(m.fileBlocks) - 1; i >= 0; i-- {
-		if m.fileBlocks[i] == "." {
-			continue
+		for fpIndex, freePosition := range m.freePositions {
+			if fileToMove.lengths <= freePosition.lengths && fileToMove.index > freePosition.index {
+
+				for l := range fileToMove.lengths {
+					m.fileBlocks[freePosition.index+l] = strconv.Itoa(fileToMove.value)
+					m.fileBlocks[fileToMove.index+l] = "."
+				}
+				m.freePositions[fpIndex].lengths -= fileToMove.lengths
+				m.freePositions[fpIndex].index += fileToMove.lengths
+				// no need to update m.filePositions, as there's only 1 try to move the files
+
+				break
+			}
 		}
-
-		if freePositionsIndex+1 == numberOfFreePositions {
-			return
-		}
-
-		nextFreePosition := m.freePositions[freePositionsIndex]
-		if nextFreePosition > i {
-			return
-		}
-
-		val := m.fileBlocks[i]
-		m.fileBlocks[nextFreePosition] = val
-		m.fileBlocks[i] = "."
-
-		freePositionsIndex++
 	}
 }
 
 func (m *Disk) calculateChecksum() error {
 	for position, fileIdString := range m.fileBlocks {
+		// Part 1: Early return
+		// if fileIdString == "." {
+		// 	return nil
+		// }
+
 		if fileIdString == "." {
-			return nil
+			continue
 		}
 
 		fileId, conversionError := strconv.Atoi(fileIdString)
@@ -97,12 +150,18 @@ func (m *Disk) unpackDiskMap() {
 			// can be larger than 10 (meaning 2 digits)
 			fileId := i / 2
 
+			m.filePositions = append(m.filePositions, FilePosition{index: len(m.fileBlocks), value: fileId, lengths: val})
+
 			for range val {
 				m.fileBlocks = append(m.fileBlocks, strconv.Itoa(fileId))
 			}
+		} else if val == 0 {
+			// empty space with lengths 0
+			continue
 		} else {
 			freeRange := makeRange(len(m.fileBlocks), len(m.fileBlocks)+val-1)
-			m.freePositions = append(m.freePositions, freeRange...)
+			m.freePositionIndices = append(m.freePositionIndices, freeRange...)
+			m.freePositions = append(m.freePositions, FreePosition{index: len(m.fileBlocks), lengths: val})
 
 			emptySpace := strings.Split(strings.Repeat(".", val), "")
 			m.fileBlocks = append(m.fileBlocks, emptySpace...)
